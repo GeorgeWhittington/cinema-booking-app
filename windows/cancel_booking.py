9679524462868f2a9794bf5027a127bc065073e4
Toby Warn <tobywarn@outlook.com>
2023-01-01 00:30:18 +0000

    just making files for remaining pages to be completed by 10th Jan

---
+++ windows/cancel_booking.py
@@ -0,0 +1,4 @@
+from tkinter import ttk
+from PIL import ImageTk, Image
+from database_models import session, Showing, Cinema, Film, Screen, Genre, AgeRatings
+from windows import FilmShowingWindow, FilmWindow
---
+++ windows/cinema_locations.py
@@ -0,0 +1,3 @@
+from tkinter import ttk
+from database_models import session, Showing, Cinema, Film, Screen, Genre, AgeRatings
+from windows import FilmShowingWindow, FilmWindow
