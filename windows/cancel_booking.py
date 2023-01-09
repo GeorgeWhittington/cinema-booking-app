b1398d46e468a908038939cd466a0ca23658076b
Toby Warn <tobywarn@outlook.com>
2023-01-09 22:19:52 +0000

    SQL alchemy implemented to cancel_booking

--- windows/cancel_booking.py
+++ windows/cancel_booking.py
@@ -2,54 +2,21 @@ from tkinter import ttk
 from database_models import session, Showing, Cinema, Film, Screen, Genre, AgeRatings, Booking
 from windows import FilmShowingWindow, FilmWindow
 
-#Alchemy code here
-
 class cancelBooking(ttk.Frame):
-
     def __init__(self, parent, *args, **kwargs):
         kwargs["padding"] = (3, 3, 3, 3)
         super().__init__(parent, *args, **kwargs)
         
         self.inspect_frame = ttk.Frame(self)
-
-        headings = [
-            "Booking Reference",
-            "Surname"
-        ]
-
-        self.treeview_frame = ttk.Treeview(self,
-            columns=tuple(i for i in range(len(headings))),
-            show="headings",
-            selectmode="browse")
-        self.treeview_scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.treeview.yview)
-        self.treeview["yscrollcommand"] = self.treeview.set
-        for i, value in enumerate(headings):
-            self.treeview.column(i, stretch=True)
-            self.treeview.heading(i, text=value)
-
-        self.treeview.bind("<<TreeviewSelect>>", self.treeview_select)
-        
         #Booking reference field
-        self.booking_reference = ttk.Entry(self.inspect_frame, width= 20, textvariable="Booking Reference: ")
-        #Surname field
-        self.booking_surname = ttk.Entry(self.inspect_frame, width= 20, textvariable="Surname: ")
-        #Validation before searching database
+        self.booking_reference = ttk.Entry(self.inspect_frame, textvariable="Booking Reference:")
         self.cancel_button = ttk.Button(self.inspect_frame, text="Cancel Booking", image=parent.delete_icon, compund="left")
-        #Deletes booking from database
 
-        self.booking_frame = ttk.Frame(self, borderwidth=5, relief="ridge", width=1000, height=1000)
-        # Pull data from db and show booking that matches booking reference
-        self.booking_data = ttk.Label(self.booking_data, text="booking details here")
+        self.booking = session.query(Booking).filter_by(Booking.id).first()
+        session.delete(Booking)
+        session.commit()
 
         # --- Gridding ---
         self.inspect_frame.grid(column=0, row=0, rowspan=3, sticky="nsew")
         self.booking_reference.grid(column=0, row=0)
-        self.booking_surname.grid(column=0, row=1)
-        self.cancel_button.grid(column=0, row=2, sticky="s")
-
-        self.booking_frame.grid(column=1, row=0, rowspan=3, sticky="nsew")
-        self.booking_data.grid(column=1, row=0)
-
-        for i in range(3):
-            self.rowconfigure(i, weight=1)
-            self.columnconfigure(2, weight=1)
+        self.cancel_button.grid(column=0, row=1, sticky="s")
